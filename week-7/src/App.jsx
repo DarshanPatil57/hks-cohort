c326fb38a09a40b9ff8f47dfd638953d2a6c924e
Darshan Patil <5303darshan@gmail.com>
2024-08-15 07:30:30 +0530

    routing and props drilling

--- week-7/src/App.jsx
+++ week-7/src/App.jsx
@@ -1,6 +1,6 @@
-import React from "react"
+import React, { Suspense } from "react"
 import { lazy } from "react"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom"
 import Home from './components/Home'
 // import About from './components/About'
 // import Contact from './components/Contact'
@@ -14,19 +14,65 @@ function App() {
 
   return (
     <>
-    <div>
-      <button onClick={() => window.location.href="/about"}>About</button>
-      <button onClick={() => window.location.href="/contact"}>Contact</button>
-    </div>
     <BrowserRouter>
+    <AppBar/>
     <Routes>
       <Route path="/" element={<Home/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/contact" element={<Contact/>}/>
+      <Route path="/about" element={ <Suspense fallback="loading..."> <About/> </Suspense>}/>
+      <Route path="/contact" element={ <Suspense fallback={"loading..."}> <Contact/> </Suspense>}/>
     </Routes>
     </BrowserRouter>
     </>
   )
 }
 
+function AppBar(){
+  const navigate = useNavigate()
+  return(
+    <div>
+    <button onClick={() => navigate("/about")}>About</button>
+    <button onClick={() => navigate("/contact")}>Contact</button>
+  </div>
+  )
+}
 export default App
+
+
+
+// Prop drilling 
+
+// todo:  interview question - Prop drilling doesn't means that parent re-renders children it just means the syntatic uneasiness when writtong the code .
+// import { useState } from "react"
+
+// export default function App(){
+//   const [count,setCount] = useState(0)
+//   return(
+//     <div>
+//       <Count count={count} setCount={setCount}/>
+//     </div>
+//   )
+// }
+
+// function Count({count,setCount}){
+//   return(
+//     <div>
+//       {count}
+//       <Button count={count} setCount={setCount}/>
+//     </div>
+//   )
+// }
+
+// function Button({count,setCount}){
+
+//   return(
+//     <div>
+//       <button onClick={() => setCount(count+1)}>
+//         Increment
+//       </button>
+
+//       <button onClick={() => setCount(count-1)}>
+//         Decrement
+//       </button>
+//     </div>
+//   )
+// }
